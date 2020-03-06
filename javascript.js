f946b0a142bcc9c959f22b4570e5842b89beecac
Grasshopper <mikaela301@gmail.com>
2020-03-05 21:00:51 -0800

    Add inital files

---
+++ javascript.js
@@ -0,0 +1,22 @@
+document.getElementById("button1").onclick = function() {
+    document.getElementById("box").style.height = "400px";
+    document.getElementById("box").style.width = "400px";
+}; 
+document.getElementById("button2").onclick = function() {
+    document.getElementById("box").style.height = "60px";
+    document.getElementById("box").style.width = "60px";
+}; 
+document.getElementById("button3").onclick = function() {
+    document.getElementById("box").style.backgroundColor = "blue"; 
+}; 
+
+document.getElementById("button4").onclick = function() {
+    document.getElementById("box").style.opacity = ".5";
+}; 
+
+document.getElementById("button5").onclick = function() {
+    document.getElementById ("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1";
+}; 
